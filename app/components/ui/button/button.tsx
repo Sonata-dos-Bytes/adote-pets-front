8a64fd01a0dec48a4c3c86a2feb46ca7ad09c404
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2025-09-22 14:59:46 -0300

    Implement structural updates and optimizations across multiple modules

--- app/components/ui/button/button.tsx
+++ app/components/ui/button/button.tsx
@@ -5,15 +5,31 @@ interface ButtonProps {
   children: React.ReactNode;
   type?: "button" | "submit" | "reset";
   onClick?: () => void;
+  bgColor?: string;
+  textColor?: string;
 }
 
 export default function Button({
   children,
   type = "button",
   onClick,
+  bgColor = "#fff3ed",  
+  textColor = "#F57B42", 
 }: ButtonProps) {
   return (
-    <button type={type} className="custom-button" onClick={onClick}>
+    <button
+      type={type}
+      onClick={onClick}
+      className="custom-button"
+      style={
+        {
+          "--bg-color": bgColor,
+          "--text-color": textColor,
+          "--border-color": textColor,
+          "--hover-border-color": textColor,
+        } as React.CSSProperties
+      }
+    >
       {children}
     </button>
   );
