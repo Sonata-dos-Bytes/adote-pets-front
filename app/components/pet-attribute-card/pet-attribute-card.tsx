b00797b9d0c561030112217dc22932f721f32ab2
thayna-bezerra <arraisthayna2106@gmail.com>
2025-09-26 13:57:14 -0300

    fix: :bug: Ajustar estilo do componente PetAttributeCard para melhor exibição do ícone e título

--- app/components/pet-attribute-card/pet-attribute-card.tsx
+++ app/components/pet-attribute-card/pet-attribute-card.tsx
@@ -13,13 +13,12 @@ export default function PetAttributeCard({
   icon,
 }: PetAttributeCardProps) {
   return (
-    <div className="relative w-[80px] h-[69px] bg-[#F9F9F9] rounded-[8px] flex flex-col items-center justify-center pt-6">
-      <div className="absolute -top-1/2 left-1/2 -translate-x-1/2 w-10 h-10 rounded-full bg-white flex items-center justify-center">
+    <div className="relative w-[81px] h-[70px] bg-[#F9F9F9] rounded-[8px] flex flex-col items-center justify-end pb-2">
+      <div className="absolute top-[-24px] left-1/2 -translate-x-1/2 w-10 h-10 rounded-full border-2 border-[#D77445] bg-white flex items-center justify-center">
         {icon}
-        <div className="absolute bottom-0 w-full h-1/2 border-b-4 border-[#D77445] rounded-b-full"></div>
       </div>
 
-      <h2 className="text-[12px] font-semibold text-gray-700 mt-2">{title}</h2>
+      <h2 className="text-[12px] font-semibold text-gray-700">{title}</h2>
       <p className="text-[14px] font-bold text-orange-500">{value}</p>
     </div>
   )
