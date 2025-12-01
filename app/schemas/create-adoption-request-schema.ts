46c355173f051e9417625063559c247dcbaa3eb2
matheuz233 <matheusaugus233@gmail.com>
2025-12-01 15:05:23 -0300

    fix: :bug: Ajusta o retorno da função para incluir status

--- app/schemas/create-adoption-request-schema.ts
+++ app/schemas/create-adoption-request-schema.ts
@@ -5,7 +5,7 @@ export const createAdoptionRequestSchema = z.object({
     .string()
     .min(1, 'O motivo é obrigatório')
     .min(20, 'O motivo deve ter pelo menos 20 caracteres')
-    .max(500, 'O motivo deve ter no máximo 500 caracteres'),
+    .max(100, 'O motivo deve ter no máximo 100 caracteres'),
 });
 
 export type CreateAdoptionRequestFormData = z.infer<
